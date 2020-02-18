ca1a8be1c2753d811c70573335cbce4ed0fc05b1
Takashi Ikki <takahashi@omicale.com>
2020-02-19 00:14:01 +0900

    react

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,20 +2,28 @@ module.exports = {
     mode: "development",
 
     // メインとなるJavaScriptファイル（エントリーポイント）
-    entry: "./src/main.ts",
+    entry: "./src/main.tsx",
+
+    // ファイルの出力設定
+    output: {
+        //  出力ファイルのディレクトリ名
+        path: `${__dirname}/dist`,
+        // 出力ファイル名
+        filename: "main.js"
+    },
 
     module: {
         rules: [
             {
-                // 拡張子 .ts の場合
-                test: /\.ts$/,
+                // 拡張子 .ts もしくは .tsx の場合
+                test: /\.tsx?$/,
                 // TypeScriptをコンパイル
                 use: "ts-loader"
             }
         ]
     },
-    // import文で .ts ファイルを解決する
+    // import文で .ts や .tsx ファイルを解決する
     resolve: {
-        extensions: [".ts"]
+        extensions: [".ts", ".tsx", ".js", ".json"]
     }
 };
