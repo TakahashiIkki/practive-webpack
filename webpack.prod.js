b60411d5a57280e6fb1746de2105800eaa3a8086
Takashi Ikki <takahashi@omicale.com>
2020-02-18 22:59:30 +0900

    webpack-dev-serverを導入してみた

---
+++ webpack.prod.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // メインとなるJavaScriptファイル（エントリーポイント）
+    // entry: `./src/index.js`,
+
+    // // ファイルの出力設定
+    // output: {
+    //     //  出力ファイルのディレクトリ名
+    //     path: `${__dirname}/dist`,
+    //     // 出力ファイル名
+    //     filename: "main.js"
+    // }
+    mode: "production"
+};
